7ff22e3c0fa49d1f6d76307501a304e5bf62db55
jessluff <jessica.luff@outlook.com>
2021-07-25 23:54:35 +1000

    Updated server.js, added schemas for Apollo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # book-search-engine
+
+update auth in client
--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const path = require('path');
+
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// Update Express.js to use Apollo server features
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
+
+//replace routes with schemas
