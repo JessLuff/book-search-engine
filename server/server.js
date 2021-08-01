f8fe48839c775e1f4465bccf898f78d313df7862
jessluff <jessica.luff@outlook.com>
2021-08-01 21:01:29 +1000

    Front end hooks updated UNTESTED

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 // Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -12,7 +13,8 @@ const app = express();
 // Create a new instance of an Apollo server with the GraphQL schema
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 });
 
 // Update Express.js to use Apollo server features
